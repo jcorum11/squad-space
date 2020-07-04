cec6e3f856b0324d932d163071be713cd5b9516c
Jacob Corum <cor15031@byui.edu>
2020-07-04 11:39:08 -0600

    refactored js and added html styling

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,7 +2,9 @@ const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
     const employee = new Employee("Jake", "12345", "jake@gmail.com");
+
     expect(employee.name).toBe("Jake");
     expect(employee.id).toBe("12345");
     expect(employee.email).toBe("jake@gmail.com");
+    
 });
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,33 +1,61 @@
 const generateCard = employee => {
-    return `<div class="card m-5" style="width: 18rem;">
+    let other = ""
+    let otherName = ""
+    switch (employee.type) {
+        case "intern":
+            other = employee.school;
+            otherName = "School"
+            break;
+        case "engineer":
+            other = employee.githubUsername;
+            otherName = "GitHub Username"
+            break;
+        case "manager":
+            other = employee.officeNumber;
+            otherName = "Office Number"
+    }
+    return `<div class="card m-4" style="width: 18rem;">
     <div class="card-body">
-        <h5 class="card-title">${employee.name}</h5>
+        <h5 class="card-title text-center">${employee.name}</h5>
         <p class="card-text">
-            email: ${employee.email}
-            id: ${employee.id}
+            Position: ${employee.type}
+        </p>
+        <p class="card-text">
+            ID: ${employee.id}
+        </p>
+        <p class="card-text">
+            Email: ${employee.email}
+        </p>
+        <p class="card-text">
+            ${otherName}: ${other}
         </p>
     </div>
 </div>`;
 }
 
 const generateHTML = employeeHtmlCards => {
-    return `<!DOCTYPE html>
-    <html lang="en">
-    
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
-    </head>
-    
-    <body>
-        <div class="container">
+return `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <header class="bg-danger jumbotron">
+        <h1 class="display-4 text-center">Squad-Space</h1>
+    </header>
+    <div class="container">
+        <div class="row d-flex justify-content-around">
             ${employeeHtmlCards}
         </div>
-    </body>
-    
-    </html>`;
+    </div>
+</body>
+
+</html>`;
 };
 
 module.exports = { generateHTML, generateCard };
